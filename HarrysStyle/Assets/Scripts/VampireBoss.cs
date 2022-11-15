d7ca7399ae0c9f29deba4848f6492de29a2f1a57
Karl Choi (RIT Student) <contactkarlchoi@gmail.com>
2022-11-15 13:55:08 -0500

    Bat attack working

--- HarrysStyle/Assets/Scripts/VampireBoss.cs
+++ HarrysStyle/Assets/Scripts/VampireBoss.cs
@@ -10,18 +10,25 @@ public class VampireBoss : MonoBehaviour
     [Header("Vampire Boss")]
     [SerializeField] private float waitTime = 3f; //cooldown for actions
     [SerializeField] private Color colorOG;
+    [SerializeField] private int currentAttack = 0; //change this number to whatever attack you want to test
     private SpriteRenderer sprite;
     private Vector2 startingPos;
     private int flightDir = 1; //if the boss is currently flying up or down
-    private int currentAttack = 1;
 
     [Header("Dash Attack")]
     [SerializeField] private float dashStartupOG = .5f; //how long it takes before the dash occurs (after locking onto a target position)
+    [SerializeField] private float dashStartup; //the current timer for the dash
     [SerializeField] private float dashSpeed = 15f;
     [SerializeField] private Color dashColor;
-    [SerializeField] private float dashStartup; //the current timer for the dash
     [SerializeField] private int dashProgress = 0;
     private Vector2 targetPos;
+
+    [Header("Summon Bats")]
+    [SerializeField] private GameObject[] batSummons;
+    [SerializeField] private float batStartingY = 5.7f;
+    [SerializeField] private bool batsSummoned = false;
+    [SerializeField] private float batSpeed = 5f;
+    [SerializeField] private Color batColor;
 
     // Start is called before the first frame update
     void Start()
@@ -39,11 +46,16 @@ public class VampireBoss : MonoBehaviour
         {
             if(currentAttack == 0)
             {
-                //randomly choose next attack
+                currentAttack = Random.Range(1, 3);
+                //currentAttack = 2;
             }
             else if(currentAttack == 1)
             {
                 DashAttack();
+            }
+            else if(currentAttack == 2)
+            {
+                SummonBats();
             }
         }
         else
@@ -78,7 +90,7 @@ public class VampireBoss : MonoBehaviour
         if(dashStartup == dashStartupOG)
         {
             targetPos = player.transform.position; //the location that the vampire boss will dash to
-            if(dashProgress == 1 && Mathf.Abs(transform.position.x - targetPos.x) > 0.5f)
+            if(dashProgress == 1 && Mathf.Abs(transform.position.x - targetPos.x) > 0.25f)
             {
                 if(transform.position.x - targetPos.x > 0f)
                 {
@@ -109,7 +121,6 @@ public class VampireBoss : MonoBehaviour
             }
             else
             {
-
                 //resets the dash
                 dashStartup = dashStartupOG;
                 //2-part dash
@@ -120,8 +131,41 @@ public class VampireBoss : MonoBehaviour
                 }
                 else
                 {
-                    waitTime = 5f;
                     dashProgress = 0;
+
+                    waitTime = 4f;
+                    currentAttack = 0;
+                    sprite.color = colorOG;
+                }
+            }
+        }
+    }
+
+    public void SummonBats()
+    {
+        if (!batsSummoned)
+        {
+            sprite.color = batColor;
+            foreach (GameObject batSummon in batSummons)
+            {
+                batSummon.transform.position = new Vector2(player.transform.position.x + Random.Range(-2f, 2f), batStartingY);
+                batSummon.SetActive(true);
+            }
+            batsSummoned = true;
+        }
+        else
+        {
+            foreach (GameObject batSummon in batSummons)
+            {
+                if(batSummon.transform.position.y > -7f)
+                {
+                    batSummon.transform.Translate(new Vector2(0, -batSpeed * Time.deltaTime));
+                }
+                else
+                {
+                    batsSummoned = false;
+                    waitTime = 2f;
+                    currentAttack = 0;
                     sprite.color = colorOG;
                 }
             }
