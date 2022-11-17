a2fb46470a12f66d7dc4d5544d373b8bc9d76379
LackOfBeef <egan@frelich.us>
2022-11-17 13:40:38 -0500

    Added some camera code and adjusted some sprite settings

--- HarrysStyle/Assets/CameraMovement.cs
+++ HarrysStyle/Assets/CameraMovement.cs
@@ -6,6 +6,9 @@ public class CameraMovement : MonoBehaviour
 {
     [SerializeField] private Transform playerTransform;
     [SerializeField] private bool scrollY;
+    [SerializeField] private float minBound;
+    [SerializeField] private float maxBound;
+    [SerializeField] private float nonScrollingPos;
 
     // Start is called before the first frame update
     void Start()
@@ -18,11 +21,35 @@ public class CameraMovement : MonoBehaviour
     {
         if(scrollY)
         {
-            transform.position = new Vector3(transform.position.x, playerTransform.position.y, -10);
+            transform.position = new Vector3(nonScrollingPos, playerTransform.position.y, -10);
+            if (transform.position.y < minBound)
+            {
+                transform.position = new Vector3(transform.position.x, minBound, transform.position.z);
+            }
+            else if (transform.position.y > maxBound)
+            {
+                transform.position = new Vector3(transform.position.x, maxBound, transform.position.z);
+            }
         }
         else
         {
-            transform.position = new Vector3(playerTransform.position.x, transform.position.y, -10);
+            transform.position = new Vector3(playerTransform.position.x, nonScrollingPos, -10);
+            if(transform.position.x < minBound)
+            {
+                transform.position = new Vector3(minBound, transform.position.y, transform.position.z);
+            }
+            else if (transform.position.x > maxBound)
+            {
+                transform.position = new Vector3(maxBound, transform.position.y, transform.position.z);
+            }
         }
+    }
+
+    public void SetCam(bool _scrollY, float _minBound, float _maxBound, float _nonScrollPos)
+    {
+        scrollY = _scrollY;
+        minBound = _minBound;
+        maxBound = _maxBound;
+        nonScrollingPos = _nonScrollPos;
     }
 }
