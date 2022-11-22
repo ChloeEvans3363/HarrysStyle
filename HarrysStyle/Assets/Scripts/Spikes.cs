84cef935a2535d49594dfe7b777896466b67d941
Chloe Evans <cre3363@rit.edu>
2022-11-22 15:16:17 -0500

    Added spikes

---
+++ HarrysStyle/Assets/Scripts/Spikes.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spikes : MonoBehaviour
+{
+    private int attackDamage = 1;
+    public GameObject checkpoint;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            if (!collision.GetComponent<CharacterController>().invincible)
+            {
+                collision.GetComponent<CharacterController>().Damage(attackDamage);
+                collision.GetComponent<CharacterController>().transform.position = checkpoint.transform.position;
+            }
+        }
+    }
+}
